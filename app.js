62fdbf0a48fdb1ce3aa6ef1bc0f65879f39c8fe6
Oscrv <ojay_v@yahoo.com>
2020-10-24 02:59:51 -0700

    fixed bug in eventListener

--- app.js
+++ app.js
@@ -59,43 +59,43 @@ function checkLetter(btn) {
     return letterMatch;
 }
 
+function checkWin(){
+    const phraseLetters = document.querySelectorAll('.letter');
+    const correctLetters = document.querySelectorAll('.show');
+
+    const winMessage = document.createElement('h2');
+    winMessage.innerText = 'You Win, Congratulations!';
+    const loseMessage = document.createElement('h2');
+    loseMessage.innerText = 'Better Luck Next Time : (';
+
+    if (phraseLetters.length === correctLetters.length) {
+        overlay.classList.add('win');
+        overlay.style.visibility = 'visible';
+        overlay.appendChild(winMessage);
+    }
+    if (missed >=  5) {
+        overlay.classList.add('lose');
+        overlay.style.visibility = 'visible';
+        overlay.appendChild(loseMessage);
+    }
+}
+
 
 qwerty.addEventListener('click', (event) => {
-    if(event.target.tagName == 'BUTTON') {
+    if(event.target.tagName === 'BUTTON') {
         event.target.classList.add('chosen');
         event.target.setAttribute("disabled", "true");
-    }
-    const letterFound = checkLetter(event.target);
+
+        const letterFound = checkLetter(event.target);
     
-    const scoreboard = document.querySelector('#scoreboard ol')
-    const tries = document.querySelectorAll('.tries');
+        const scoreboard = document.querySelector('#scoreboard ol')
+        const tries = document.querySelectorAll('.tries');
 
-    if (letterFound === null) {
-        missed ++;
-        scoreboard.removeChild(tries[0]);
-    }
-
-    function checkWin(){
-        const phraseLetters = document.querySelectorAll('.letter');
-        const correctLetters = document.querySelectorAll('.show');
-
-        const winMessage = document.createElement('h2');
-        winMessage.innerText = 'You Win, Congratulations!';
-        const loseMessage = document.createElement('h2');
-        loseMessage.innerText = 'Better Luck Next Time : (';
-
-        if (phraseLetters.length === correctLetters.length) {
-            overlay.classList.add('win');
-            overlay.style.visibility = 'visible';
-            overlay.appendChild(winMessage);
-        }
-        if (missed >=  5) {
-            overlay.classList.add('lose');
-            overlay.style.visibility = 'visible';
-            overlay.appendChild(loseMessage);
+        if (letterFound === null) {
+            missed ++;
+            scoreboard.removeChild(tries[0]);
         }
     }
     checkWin();
-
 });
 
